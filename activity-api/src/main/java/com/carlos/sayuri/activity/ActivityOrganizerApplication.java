832b03f3df513009a0cbcc150f38fd07b5c894d6
Sayuri Takano <sayutkn@gmail.com>
2024-10-01 21:44:37 -0300

    springboot initializer

---
+++ activity-api/src/main/java/com/carlos/sayuri/activity/ActivityOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.sayuri.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityOrganizerApplication.class, args);
+	}
+
+}
---
+++ activity-api/src/test/java/com/carlos/sayuri/activity/ActivityOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.sayuri.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
